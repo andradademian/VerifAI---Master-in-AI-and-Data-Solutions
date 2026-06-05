9243b7f351de62eb29599bb859285448bdf06294
Andrada <andradademian@gmail.com>
2026-06-05 18:14:00 +0200

    Fixed env

--- App/app.py
+++ App/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import requests
 import os
 from dotenv import load_dotenv
+import cohere
 
 # -------------------------------------------------------------------
 # AI MODEL IMPORTS
